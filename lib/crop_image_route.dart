915b5f6249ff6ae001b9c2769761e29e84c4d847
dingjianjaja <dingjianjaja@qq.com>
2020-05-19 16:28:08 +0800

    接口实现

--- lib/crop_image_route.dart
+++ lib/crop_image_route.dart
@@ -6,6 +6,9 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:image_crop/image_crop.dart';
 import 'package:image/image.dart' as ty;
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:convert';
 
 class CropImageRoute extends StatefulWidget {
   CropImageRoute(this.image);
@@ -26,6 +29,7 @@ class _CropImageRouteState extends State<CropImageRoute> {
   bool selectDone = false;
   File selectImage;
   ty.Image newSizeImage;
+  List totalDataList;
 
   @override
   Widget build(BuildContext context) {
@@ -36,8 +40,8 @@ class _CropImageRouteState extends State<CropImageRoute> {
       color: Colors.blue,
       child: selectDone
           ? CustomPaint(
-            painter: TYPainter(context, imageView),
-          )
+              painter: TYPainter(context, imageView,totalDataList),
+            )
           : Column(
               children: <Widget>[
                 Container(
@@ -78,9 +82,7 @@ class _CropImageRouteState extends State<CropImageRoute> {
         }).catchError(() {
           print('裁剪不成功');
         });
-      } else {
-        upload(originalFile);
-      }
+      } else {}
     });
   }
 
@@ -88,18 +90,22 @@ class _CropImageRouteState extends State<CropImageRoute> {
   Future<String> changeImageSizeToBase64String(File file) async {
     /// 获取原图
     ty.Image image = ty.decodeImage(file.readAsBytesSync());
+
     /// 修改为制定像素大小
     ty.Image newImage = ty.copyResize(image, width: 608);
+
     /// 转化为base64格式
     String imgString = base64Encode(ty.encodeJpg(newImage));
 
-    /// 完成后更新UI
-    selectDone = true;
-    selectImage = file;
+
 
     imageView = await loadImageByUint8List(ty.encodeJpg(newImage));
 
-    setState(() {});
+    File newFile = File(await _findFileDownloadLocalPath('dingjian.jpg'));
+    newFile.writeAsBytesSync(ty.encodeJpg(newImage));
+
+    upload(newFile);
+
 
     return imgString;
   }
@@ -113,56 +119,135 @@ class _CropImageRouteState extends State<CropImageRoute> {
 
   ///上传
   void upload(File file) {
-    print(file.path);
     Dio dio = Dio();
     dio
-        .post("http://your ip:port/", data: FormData.fromMap({'file': file}))
+        .post("http://192.168.0.230:8080",
+            data: FormData.fromMap(
+                {'file': MultipartFile.fromFileSync(file.path)}))
         .then((response) {
       if (!mounted) {
         return;
       }
       //处理上传结果
-      print('上传头像结果 ${response.data}');
+      print('上传结果 ${response.data}');
+      String resposeStr = response.data;
+
+      var tempMap = jsonDecode(resposeStr);
+      String listStr = tempMap['res'];
+      String newStr = listStr.replaceAll("'", '\"');
+      List tempArr = json.decode(newStr);
+
+      print(tempArr);
+
+      totalDataList = tempArr;
+      /// 完成后更新UI
+      selectDone = true;
+      selectImage = file;
+      setState(() {
+      });
+
     });
+  }
+
+  /// 申请权限
+  Future<bool> _checkPermission() async {
+    /// 先对所在平台进行判断
+    if (Theme.of(context).platform == TargetPlatform.android) {
+      PermissionStatus permission = await PermissionHandler()
+          .checkPermissionStatus(PermissionGroup.storage);
+      if (permission != PermissionStatus.granted) {
+        Map<PermissionGroup, PermissionStatus> permissions =
+            await PermissionHandler()
+                .requestPermissions([PermissionGroup.storage]);
+        if (permissions[PermissionGroup.storage] == PermissionStatus.granted) {
+          return true;
+        }
+      } else {
+        return true;
+      }
+    } else {
+      return true;
+    }
+    return false;
+  }
+
+  /// 获取设备主存储路径
+  Future<String> _findLocalPath() async {
+    await _checkPermission();
+
+    /// 因为Apple没有外置存储，所以第一步我们需要先对所在平台进行判断
+    /// 如果是android，使用getExternalStorageDirectory
+    /// 如果是iOS，使用getApplicationSupportDirectory
+    final directory = Theme.of(context).platform == TargetPlatform.android
+        ? await getExternalStorageDirectory()
+        : await getApplicationSupportDirectory();
+    return directory.path;
+  }
+
+  /// 获取文件下载存储路径
+  Future<String> _findFileDownloadLocalPath(String fileName) async {
+    String mainPath = await _findLocalPath();
+    var file = Directory(mainPath + "/" + "download/");
+    try {
+      bool exists = await file.exists();
+      if (!exists) {
+        await file.create();
+      }
+    } catch (e) {
+      print(e);
+    }
+    return file.path + fileName;
   }
 }
 
 class TYPainter extends CustomPainter {
   ui.Image image;
+  List dataList = [];
 
   Paint tyPaint = Paint();
   final BuildContext context;
   Color color = Colors.red;
 
-  TYPainter(this.context, this.image) {
+  TYPainter(this.context, this.image, this.dataList) {
     this.tyPaint = Paint();
   }
 
   @override
   void paint(Canvas canvas, Size size) {
-    double scale = size.width / image.width *1;
-
+    double scale = size.width / image.width * 1;
     canvas.drawImageRect(
         image,
         Rect.fromLTWH(0, 0, image.width * 1.0, image.height * 1.0),
-        Rect.fromLTWH(0, 0, scale*image.width, scale*image.height),
+        Rect.fromLTWH(0, 0, scale * image.width, scale * image.height),
         tyPaint);
-    canvas.drawRect(
-        Rect.fromLTWH(140 *scale, 110*scale, 30*scale, 130*scale),
-        tyPaint
-          ..style = PaintingStyle.stroke
-          ..color = Colors.red);
 
-    TextPainter textPainter = TextPainter(
-        textAlign: TextAlign.center,
-        maxLines: 1,
-        text: TextSpan(
-          text: 'RRU',style: TextStyle(color: Colors.red),
-        ),
-        textDirection: TextDirection.rtl);
-    textPainter
-      ..layout(maxWidth: 50, minWidth: 50)
-      ..paint(canvas, Offset(200*scale, 100*scale));
+    /// 根据返回检测点 进行绘制
+    for (List item in dataList) {
+      String title = item[0];
+      double x = item[2];
+      double y = item[3];
+      double w = item[4];
+      double h = item[5];
+
+      canvas.drawRect(
+        Rect.fromCenter(center: Offset(x * scale, y * scale),width: w * scale,height: h * scale),
+          tyPaint
+            ..style = PaintingStyle.stroke
+            ..color = Colors.red
+            ..strokeWidth = 2);
+
+      TextPainter textPainter = TextPainter(
+          textAlign: TextAlign.center,
+          maxLines: 1,
+          text: TextSpan(
+            text: title,
+            style: TextStyle(color: Colors.red),
+          ),
+          textDirection: TextDirection.rtl);
+      textPainter
+        ..layout(maxWidth: 150, minWidth: 50)
+        ..paint(canvas, Offset(x * scale, (y - 20) * scale));
+    }
   }
 
   @override
